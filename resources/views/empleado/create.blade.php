6b396521986f7ab8b2c5691d450d6afcd31a7817
Mairon <mairon199917@gmail.com>
2022-07-21 00:56:48 -0400

    Subida de proyecto

---
+++ resources/views/empleado/create.blade.php
@@ -0,0 +1,8 @@
+Formulario de creación de empleado
+
+<form action="{{url('/empleado')}}" method="post" enctype="multipart/form-data">
+@csrf
+@include('empleado.form',['modo'=>'Crear']);
+
+
+</form>
---
+++ resources/views/empleado/index.blade.php
@@ -0,0 +1,55 @@
+Mostrar la lista de empleados
+@if(Session::has('mensaje'))
+{{Session::get('mensaje')}}
+
+@endif
+
+<a href= "{{url('empleado/create')}}"> Registrar nuevo empleado</a>
+<table class="table table-secondary">
+    <thead class="thead-info">
+        <tr>
+            <th>#</th>
+            <th>Foto</th>
+            <th>Nombre</th>
+            <th>Apellido Paterno</th>
+            <th>Apellido Materno</th>
+            <th>Correo</th>
+            <th>Acciones</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach($empleados as $empleado)
+        <tr>
+            <td>{{$empleado->id}}</td>
+
+            <td>
+                <img src="{{asset('storage').'/'.$empleado->Foto }}" width="100" alt="">
+            
+            </td>
+
+
+            <td>{{$empleado->Nombre}}</td>
+            <td>{{$empleado->ApellidoPaterno}}</td>
+            <td>{{$empleado->ApellidoMaterno}}</td>
+            <td>{{$empleado->Correo}}</td>
+            <td>
+                <a href="{{url('/empleado/'.$empleado->id.'/edit')}}">
+                Editar 
+                </a>
+
+                | 
+
+                <form action="{{url('/empleado/'.$empleado->id)}}" method="post">
+                @csrf  
+                {{method_field('delete')}}  
+                <input type="submit" onclick="return confirm('¿Quieres borrar?')" value="Borrar">
+                
+                </form>
+
+            
+            
+            </td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
